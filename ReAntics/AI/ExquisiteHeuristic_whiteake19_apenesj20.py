7d9a1be3a8ee2f84a1f9d61bfeb15f35f1619ed4
Noah <nschutte95@gmail.com>
2018-10-06 18:14:33 -0700

    Finale before final version

--- ReAntics/AI/ExquisiteHeuristic_whiteake19_apenesj20.py
+++ ReAntics/AI/ExquisiteHeuristic_whiteake19_apenesj20.py
@@ -29,7 +29,7 @@ class AIPlayer(Player):
     #   cpy           - whether the player is a copy (when playing itself)
     ##
     def __init__(self, inputPlayerId):
-        super(AIPlayer,self).__init__(inputPlayerId, "ExquisiteHeuristic")
+        super(AIPlayer,self).__init__(inputPlayerId, "Max")
         self.depth_limit = 3
         self.me = 0
         self.move = None
@@ -120,8 +120,9 @@ class AIPlayer(Player):
         else:
             # so move is not None AND move is our move
             self.nextMove = self.move["next-move"]
-        if self.prunedMoves != 0:
-            print("Pruned ", self.prunedMoves, " moves")
+        # if you want the number of pruned moves to be printed, use the two lines below
+        #if self.prunedMoves != 0:
+        #    print("Pruned ", self.prunedMoves, " moves")
         return self.move["move"]
 
     ##
@@ -144,6 +145,7 @@ class AIPlayer(Player):
     # however we need to reset global variables
     #
     def registerWin(self, hasWon):
+        # reset these variables so it does not interfere with the next game
         self.move = None
         self.nextMove = None 
         pass
@@ -155,6 +157,7 @@ class AIPlayer(Player):
     #
     def evaluateState(self, currentState):
         me = self.me
+        # If we win or lose, just return the right value straight away
         if getWinner(currentState) == 1:
             return 1
         elif getWinner(currentState) == 0:
@@ -162,38 +165,43 @@ class AIPlayer(Player):
         # Get the different inventories
         myInventory = currentState.inventories[me]
         enemyInventory = currentState.inventories[1-me]
-        neutralInventory = currentState.inventories[2]
-        ourAnts = getAntList(currentState, me, types=(QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER))
+        neutralInventory = currentState.inventories[2] 
+        # Queen should stay around the anthill
         ourQueen = getAntList(currentState, me, types=(QUEEN,))
+        queenAroundAnthill = 20
         if len(ourQueen) == 1:
             ourAnthillCoords = myInventory.getAnthill().coords
             queenAroundAnthill = max(approxDist(ourQueen[0].coords, ourAnthillCoords),2)
+        # Never get more than 3 ants
+        ourAnts = getAntList(currentState, me, types=(QUEEN, WORKER, DRONE, SOLDIER, R_SOLDIER))
         if len(ourAnts) == 4:
             return -1
+        # One worker is more than enough. Highest priority ant.
         workers = getAntList(currentState, me, types=(WORKER, WORKER))
         if len(workers) == 1:
             worker = 1
         else:
             worker = 0
-        # worker = min(len(workers), 1) # One or zero, we only want one worker
+        # One ranged soldier is more than enough. Priority one below the worker.
         rSoldiers = getAntList(currentState, me, types=(R_SOLDIER, R_SOLDIER))
-        rSoldier = min(len(rSoldiers), 1) # One or zero, we only want one ranged soldier
-        # If we have an r soldier make sure it attacks a worker or goes to the opponent's anthill
+        if len(rSoldiers) == 1:
+            rSoldier = 1
+        else:
+            rSoldier = 0
+        # If we have an r soldier make sure it attacks a worker or goes to the opponent's queen
         rSoldierDistance = 20
+        enemyQueen = getAntList(currentState, 1-me, types=(QUEEN,))
         if rSoldier:
             enemyWorkers = getAntList(currentState, 1-me, types=(WORKER, WORKER))
             if enemyWorkers:
                 rSoldierDistance = approxDist(rSoldiers[0].coords, enemyWorkers[0].coords)
             else: 
-                queen = getAntList(currentState, 1-me, types=(QUEEN,))
-                if len(queen) == 1:
-                    enemyQueenCoords = queen[0].coords
+                if len(enemyQueen) == 1: # Just to double check we actually have a queen
+                    enemyQueenCoords = enemyQueen[0].coords
                     rSoldierDistance = abs(approxDist(rSoldiers[0].coords, enemyQueenCoords)-3)
                 else:
                     return 1
-                    #enemyAnthillCoords = enemyInventory.getAnthill().coords
-                    #rSoldierDistance = abs(approxDist(rSoldiers[0].coords, enemyAnthillCoords)-3)
-        # Make sure the worker goes to get food and gets it back to the anthill (or tunnel)
+        # Make sure the worker goes to get food and gets it back to the tunnel
         foodDistance = 20
         # Incentivize carrying food
         carrying = 0
@@ -209,15 +217,14 @@ class AIPlayer(Player):
                         newDistance = approxDist(workers[0].coords, foodCoords)
                         if newDistance <= foodDistance:
                             foodDistance = newDistance
-                # foodDistance = max(approxDist(workers[0].coords, foodCoords),1)
             else:  # Compute distance to tunnel if carrying
                 myTunnelCoords = myInventory.getTunnels()[0].coords
                 foodDistance = approxDist(workers[0].coords, myTunnelCoords)
-        # Get the other used parameters
-        queenList = getAntList(currentState, 1-me, types = (QUEEN, QUEEN))
+        # Enemy queen health is also important
         enemyQueenHealth = 0
-        if queenList:
-            enemyQueenHealth = queenList[0].health
+        if len(enemyQueen) == 1:
+            enemyQueenHealth = enemyQueen[0].health
+        # Get food and enemy ant number
         ourFoodNumber = myInventory.foodCount
         enemyAntNumber = len(enemyInventory.ants)
         enemyFoodNumber = enemyInventory.foodCount
@@ -226,7 +233,6 @@ class AIPlayer(Player):
         enemyPoints = 3*enemyFoodNumber + 2*enemyAntNumber + enemyQueenHealth
         # This makes sure the value is always between -1 and 1
         value = (ourPoints - enemyPoints) / (ourPoints + enemyPoints) 
-        # If the AI wins the value should be 1, otherwise it should be -1
         return value
 
     ##
@@ -257,18 +263,19 @@ class AIPlayer(Player):
     ##
     # evalListNodes
     #
-    # This function takes a list of nodes and takes the average of the values accossiated with
-    # each and returns that average value
+    # This function takes a list of nodes and takes the best node dependent on 
+    # the move being a min or max move. If a node has a "minmax" of 1 it is a max move,
+    # if a node has a "minmax" of -1 it is a min move.
     #
     def evalListNodes(self, nodes):
         if nodes and len(nodes) > 1:
             randomNode = nodes[0]
-            if randomNode["minmax"] == 1:
+            if randomNode["minmax"] == 1: 
                 bestNodeValue = -1
                 for node in nodes:
                     if node["value"] >= bestNodeValue:
                         bestNodeValue = node["value"]
-            elif randomNode["minmax"] == -1:
+            elif randomNode["minmax"] == -1: 
                 bestNodeValue = 1
                 for node in nodes:
                     if node["value"] <= bestNodeValue:
@@ -286,20 +293,23 @@ class AIPlayer(Player):
     # This function preforms minimax search
     # It takes a node and the depth
     # The nodes are expanded until the depth limit is reached
-    # Then the values of the nodes are averaged and propagated up
-    # The move from the node with the best value at depth 0 is returned as the best move to make
+    # Then the values of the nodes are propagated up based on being the best min or max move
+    # The method returns a node that contains a squence of moves (when depth == 0)
+    # This assumes both players are rational
+    # Alpha-beta pruning is used to make this process faster by pruning nodes that for certain
+    # do not hold better outcomes
     #
     def minimax(self, node, depth):
         newNodes = self.expandNode(node)
         shuffle(newNodes)
-        # create counter
+        # create pruning counter to see how many nodes get pruned
         counter = 0
         # it is depth + 1 since we just expanded the node and are
         # now evaluating nodes at depth + 1
         if depth+1 < self.depth_limit:
             # if the next set of nodes are inside the depth limit,
             for n in newNodes:
-                # increment counter
+                # increment pruning counter
                 counter += 1
                 # update the bounds of each newNode since a previous newNode could have updated node's bounds
                 n["min"] = node["min"]
@@ -311,15 +321,16 @@ class AIPlayer(Player):
                     # if the value was updated, update the next-move value to n
                     if temp != node["min"]:
                         node["next-move"] = n
-                        # if the bounds cross each other, prune remaining nodes
-                    if node["min"] > node["max"] or node["min"] == 1:
+                    # if the bounds cross each other, prune remaining nodes
+                    # if min bound equals 1, just return it
+                    if node["min"] > node["max"] or node["min"] == 1: 
                         # updated global variable
                         self.prunedMoves += len(newNodes) - counter
                         if depth == 0:
                             return node
                         else: 
                             return node["min"]
-                else:
+                else: # here the same happens for "minmax" == -1
                     temp = node["max"]
                     node["max"] = min(self.minimax(n, depth+1), node["max"])
                     if temp != node["max"]:
@@ -333,7 +344,7 @@ class AIPlayer(Player):
         else:
             # else find the best value for min/max
             for n in newNodes:
-                # increment counter
+                # increment pruning counter
                 counter += 1
                 if node["minmax"] == 1:
                     temp = node["min"]
@@ -347,7 +358,7 @@ class AIPlayer(Player):
                         node["next-move"] = n
                     if node["min"] == 1:
                         return node["min"]
-                else:
+                else: # here the same happens for "minmax" == -1
                     temp = node["max"]
                     node["max"] = min(self.evaluateState(n["state"]), node["max"])
                     if node["min"] > node["max"]:
@@ -356,88 +367,12 @@ class AIPlayer(Player):
                     # if the value was updated, update the next-move value to n
                     if temp != node["max"]:
                         node["next-move"] = n
-        # evaluation = self.evalListNodes(newNodes) NOT NEEDED, minimax() now evaluates nodes
+        # if we are not at depth 0 we return a value, otherwise we return a node
         if depth > 0:
             if node["minmax"] == 1:
                 return node["min"]
             else:
                 return node["max"]
         else:
-            # shuffle(newNodes)
-            # sortedNodes = sorted(newNodes, key=lambda k: k["value"])
-            # return sortedNodes[len(sortedNodes)-1]["move"]
-
             # when we've finished minimax, return the root node with all the updated values
             return node
-
-################
-#  UNIT TESTS  #
-################
-
-
-ai = AIPlayer(0)
-
-# create general state to modify
-testState = GameState.getBasicState()
-# make it an average game state
-# add food to (3,3), (6,3), (6,6), (3,6)
-testState.inventories[NEUTRAL].constrs.append(Construction((3, 3), FOOD))
-testState.inventories[NEUTRAL].constrs.append(Construction((6, 3), FOOD))
-testState.inventories[NEUTRAL].constrs.append(Construction((6, 6), FOOD))
-testState.inventories[NEUTRAL].constrs.append(Construction((3, 6), FOOD))
-# add worker next to food (2,3)
-testState.inventories[0].ants.append(Ant((2, 3), WORKER, 0))
-# make an enemy worker on opposite side
-testState.inventories[1].ants.append(Ant((7, 6), WORKER, 1))
-# add another friendly worker to look more like AI
-testState.inventories[0].ants.append(Ant((7, 3), WORKER, 0))
-# move queen so she doesn't stand on anthill and cause evaluateList() to return -1.0
-queenMove = Move(MOVE_ANT, [(0, 0), (1, 0)])
-testState = getNextState(testState, queenMove)
-
-# EVALUATE STATE TESTS
-# (1) Will worker move onto food?
-# get state with ant on food
-foodMove = Move(MOVE_ANT, [(7, 3), (6, 3)], None)
-foodState = getNextState(testState, foodMove)
-# if moving worker onto food is considered worse than not, ERROR
-if ai.evaluateState(foodState) < ai.evaluateState(testState):
-    print("ERROR: Ant will not move onto food")
-
-# (2) Will game make new ants?
-# update foodCounts
-testState.inventories[0].foodCount = 2
-testState.inventories[1].foodCount = 2
-# use food to build Soldier
-soldierBuild = Move(BUILD, None, SOLDIER)
-buildState = getNextState(testState, soldierBuild)
-# unit test not applicable with heuristic
-# if ai.evaluateState(buildState) < ai.evaluateState(testState):
-#     print("ERROR: AI will not build soldier")
-
-# (3) Will game move non-workers towards enemy queen?
-soldierMove = Move(MOVE_ANT, [(0,0),(0,1),(0,2)])
-attackState = getNextState(buildState, soldierMove)
-if ai.evaluateState(attackState) < ai.evaluateState(buildState):
-    print("ERROR: AI will not attack queen")
-
-# minimax TESTS
-# (1) Does reaching depth limit return an integer?
-testNode = {"move": None, "state": testState, "value": 0, "min": -1000, "max": 1000, "parent": None, "depth": 3,
-            "minmax": 1, "next-move": None}
-if not isinstance(ai.minimax(testNode, testNode["depth"]), float):
-    print("ERROR: minimax() doesn't test depth limit properly")
-
-# EVALLISTNODES TESTS
-# (1) Does sending an empty list cause a DivideByZeroError?
-try:
-    ai.evalListNodes([])
-except:
-    print("ERROR: evalListNodes() cannot handle empty list input")
-
-# EXPAND NODE TESTS
-# (1) Does expanding a node return more nodes?
-testNode = {"move": None, "state": testState, "value": 0, "min": -1000, "max": 1000, "parent": None, "depth": 0,
-            "minmax": 1, "next-move": None}
-if len(ai.expandNode(testNode)) == 0:
-    print("ERROR: expandNode returns empty list")
